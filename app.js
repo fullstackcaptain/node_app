aa80b70d0c6453a023bd395f58dc4a842013e159
shahidshaikhleaque <shahidshaikhleaque@gmail.com>
2020-09-11 13:09:59 +0530

    committing for changes in package.json and app.js

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+const port = process.env.PORT || 3000;
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -18,4 +19,4 @@ app.use((req, res, next) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(process.env.PORT || 3000);
+app.listen(port);
